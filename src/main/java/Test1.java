6cdbe165836d69c167fd6d7dc08726f7d6c1a1cd
dell <sonia.gupta@franciscanecare.net>
2017-11-17 12:15:52 +0530

    committed in b1

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,6 +1,7 @@
 public class Test1 {
     public static void main(String... args){
-        System.out.println("hello");
+        System.out.println("hello java");
+        System.out.println("for testing");
         System.out.println("hello1");
         System.out.println("hello2");
     }
